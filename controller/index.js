c704b51362effdb3e961361296f5041ae487389a
cburnside1385 <chrisburnside13@gmail.com>
2022-11-05 09:02:13 -0400

    added schema

--- controller/index.js
+++ controller/index.js
@@ -1,1 +1,13 @@
+const router = require("express").Router();
+
 //add code later
+
+
+
+
+router.use((req, res) => {
+    res.status(404).end();
+});
+
+module.exports = router;
+
---
+++ models/schema.sql
@@ -0,0 +1,6 @@
+
+DROP DATABASE IF EXISTS moduledb; 
+CREATE DATABASE moduledb; 
+
+DROP DATABASE IF EXISTS moduledb; 
+CREATE DATABASE moduledb; 
